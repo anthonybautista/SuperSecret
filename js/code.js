00a686d144d93317743415e5732c7f30a082a78c
Anthony Bautista <anthonya.bautista@gmail.com>
2022-09-05 23:33:36 -0500

    add .fire

--- js/code.js
+++ js/code.js
@@ -1,3 +1,29 @@
+const FIRE = '[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"string","name":"username","type":"string"}],"name":"activate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"username","type":"string"}],"name":"addressFor","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"string","name":"username","type":"string"}],"name":"deactivate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"nftNameService","type":"address"},{"internalType":"bool","name":"allowed_","type":"bool"}],"name":"setAllowed","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"usernameFor","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}]';
+const FIRE_ADDRESS = "0x5adCD28C08Fdc5a913982391cebD866b27C717D4";
+const RPC = 'https://api.avax.network/ext/bc/C/rpc';
+
+const checkFire = async (address) => {
+    const t = new ethers.providers.JsonRpcProvider(RPC);
+    let fireContract = new ethers.Contract(FIRE_ADDRESS, FIRE, t);
+
+    let fire = await fireContract.usernameFor(address);
+
+    $("#leaderboard tr").each(function() {
+        let tdElement = $(this).children()[1];
+        if ($(tdElement).text() === address) {
+            if (fire !== "") {
+                $(tdElement).text(fire);
+            }
+        }
+    });
+}
+
+const updateFire = async () => {
+    $("#leaderboard tr").each(function() {
+        let tdElement = $(this).children()[1];
+        checkFire($(tdElement).text());
+    });
+}
 
 function getLeaderboard() {
     $("#leaderboard").empty();
@@ -5,7 +31,7 @@ function getLeaderboard() {
         `<tr><th>Rank</th><th style="width: 60%">Owner</th><th>Points</th></tr>`
     );
     $.ajax({
-        url: `https://supersecret.anthonybautist2.repl.co/api`,
+        url: `https://zombieleaderboard.m5k.repl.co/api`,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
@@ -30,5 +56,8 @@ $(function () {
     getLeaderboard();
     $("#refresh-icon").on('click', function () {
         getLeaderboard();
+    });
+    $("#fire-icon").on('click', function () {
+        updateFire();
     });
 })
